5d9e154b483ee03f67ab3421a9c1c720ee708f93
Arun <arun.prathivadhi.bhayankaram@oracle.com>
2023-07-08 16:54:43 +0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,19 @@
 terraform {
 
   cloud {
-    organization = "organization-name"
+    organization = "oratraining"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "oratraining"
     }
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.16.0"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.2.0"
 }
